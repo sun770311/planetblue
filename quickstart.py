6e4d3f75750abeee83bf3c03ad248e16e19c48ac
Hannah Sun <hysun@umich.edu>
2023-04-07 19:55:01 -0400

    removed excess comments

--- quickstart.py
+++ quickstart.py
@@ -1,65 +1,34 @@
 from __future__ import print_function
 
 import os.path
+import json
+import numpy as np
+
+import requests
+import os
+import json
+from dotenv import load_dotenv
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import json
 
 from pprint import pprint
 from googleapiclient import discovery
 
-import numpy as np
-import pandas as pd
-
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1Nubu0jvhPI1nNYZt8XuZnuTZjhmIT4flz3QR0AZwrU8'
 SAMPLE_RANGE_NAME = '!A1:B'
 
-
-# Importing required libraries
-
-
-# If you want to know in more detail, read below. If you don't understand what's written below, don't worry about it.
-
-
-# The requests library (https://requests.readthedocs.io/en/latest/) is used for making HTTP requests to the API endpoint.
-# The os library is used for accessing environment variables
-# The dotenv library (https://pypi.org/project/python-dotenv/) is used for loading variables from a .env file into the environment.
-import requests
-import os
-import json
-from dotenv import load_dotenv
-
-
-
-# The load_dotenv() function is called to load environment variables from a .env file.
-
-
-# If you want to know in more detail, read below. If you don't understand what's written below, don't worry about it.
-
-
-# This file should contain the CLIENT_ID and CLIENT_SECRET variables which are used for authentication when making requests to the API.
-# These variables are loaded into the environment using the os.getenv() function.
 load_dotenv()
 
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-
-
-
-# The requests.post() function is used to make a POST request to the API endpoint to obtain an access token.
-
-
-# If you want to know in more detail, read below. If you don't understand what's written below, don't worry about it.
-
 
 # This token is necessary for making requests to protected endpoints.
 # The grant_type parameter is set to "client_credentials" to indicate that we are using our own credentials.
@@ -89,8 +58,6 @@ token = response.json()["access_token"]
 # The Authorization header is set to include the access token in a Bearer token format.
 # The Accept header is set to "application/json" to indicate that the client expects a JSON response.
 def get_members(id1):
-    #members_url = f"https://gw.api.it.umich.edu/um/MCommunityGroups/Members/{group}"
-    #members_url = f"https://gw.api.it.umich.edu/um/mcomm-person/find/{id1}"
     members_url = f"https://gw.api.it.umich.edu/um/studentrecords/Affiliation"
     headers = {
         "Authorization": f"Bearer {token}",
@@ -102,25 +69,21 @@ def get_members(id1):
 # Finally, the requests.get() method is used to make a GET request to the group members API endpoint.
 # The headers dictionary is passed as an argument to provide the authorization and accept headers.
 # The response text is returned by the function.
-
-
     response = requests.get(members_url, headers = headers, params=parameters)
-
-    #print(response.text)
-    #print(response.json())
 
     #prints entire API response
     #print(json.dumps(response.json(), separators=(",",":"), indent=2))
 
-    #we only want the information inside of rows- QUESTION: do we want to rewrite for every
-    #single piece of personal information (first name, last name, etc.)?
+    #QUESTION: do we want to rewrite for every single piece of personal information (first name, last name, etc.)?
     json_str = json.dumps(response.json()["data"])
     resp = json.loads(json_str)
     resp_2 = resp["query"]
     resp_3 = resp_2["rows"]
     return (json.dumps(resp_3[0]["FIRST_NAME"]))
 
-my_list2 = ["hysun", "yangjust", "jmfree"]
+my_list2 = ["hysun", 
+            "yangjust", 
+            "jmfree"]
 
 
 def main():
@@ -149,13 +112,13 @@ def main():
         service = discovery.build('sheets', 'v4', credentials=creds)
 
         # The ID of the spreadsheet to update.
-        spreadsheet_id = '1Nubu0jvhPI1nNYZt8XuZnuTZjhmIT4flz3QR0AZwrU8'  # TODO: Update placeholder value.
+        spreadsheet_id = '1Nubu0jvhPI1nNYZt8XuZnuTZjhmIT4flz3QR0AZwrU8'  
 
         # The A1 notation of the values to update.
-        range_ = 'A1:A3'  # TODO: Update placeholder value.
+        range_ = 'A1:A3'  
 
         # How the input data should be interpreted.
-        value_input_option = 'USER_ENTERED'  # TODO: Update placeholder value.
+        value_input_option = 'USER_ENTERED'  
 
 
         value_range_body = {
@@ -171,9 +134,7 @@ def main():
     
         response = request.execute()
 
-        # TODO: Change code below to process the `response` dict:
         pprint(json.dumps(response))
-
 
     except HttpError as err:
         print(err)
@@ -181,12 +142,10 @@ def main():
     
 hi = []
 
-
-
 if __name__ == '__main__':
     for i in my_list2:
         hi.append(str(get_members(i)))
-        
+
     hi = np.reshape(hi, (3,1)).tolist()
     print(hi)
     main()
