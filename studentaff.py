a0784f93be8ac33a6dbe14b1d169f65e9f79bc53
Hannah Sun <hysun@umich.edu>
2023-07-20 21:01:08 +0800

    add. comments

--- studentaff.py
+++ studentaff.py
@@ -132,9 +132,12 @@ def get_affiliation(id1, target):
 # main() writes the values returned from get_affiliation() to a spreadsheet 
 def main(target, start_row, end_row):
     creds = None
-    
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
